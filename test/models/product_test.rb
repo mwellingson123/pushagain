4cde5a6f3ac2be311925514dee3e426f2cf0ab1f
mwellingson123 <michael.ellingson@pluto.dsu.edu>
2014-02-16 19:12:17 -0900

    Assignment4 Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+fixtures :products
+def new_product(image_url)
+test "product is not valid without a unique title - il8n" do
+  product = Product.new(title:	products(:ruby).title,
+			  description:	"yyy",
+			  price:		1,
+			  image_url:	"fred.gif")
+assert product.invalid?
+assert_equal [Il8n.translate(errors.messages.taken')],
+				product.errors[:title]
+end
+end
+test "image url" do
+	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+			 http://a.b.c/x/y/z/fred.gif }
+	bad = %w{ fred.doc fred.gif/more fretd.gif.more }
+	ok.each do |name|
+		assert new_product(name).valid?, "#{name} should be valid"
+		end
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+end
+		
 end
