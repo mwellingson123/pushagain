43442d187f1cd9f34ae8108e8b6aa3d009cfb7be
mwellingson123 <michael.ellingson@pluto.dsu.edu>
2014-03-04 15:42:00 -0900

    Chapter 10 Complete

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+  
   # GET /carts
   # GET /carts.json
   def index
@@ -51,12 +52,14 @@ class CartsController < ApplicationController
     end
   end
 
-  # DELETE /carts/1
+  # DELETE /carts/1'
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+	session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, 
+		notice: 'Your cart is currently empty'}
       format.json { head :no_content }
     end
   end
@@ -70,5 +73,10 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
+	  
+	def invalid_cart
+		logger.error "Attempt to access invalid cart #{params[:id]}"
+		redirect_to store_url, notice: 'Invalid cart'
+	end
     end
 end
